bb1dadc439721f182819ccf9914d93c454b25b74
hackprime <hackprime@gmail.com>
2016-03-09 22:10:09 +0700

    Bugfix: TypeError: must be psycopg2.extensions.connection, not None

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -6,5 +6,5 @@ from django.utils.text import force_text
 
 def adapt(text):
     a = psycopg2.extensions.adapt(force_text(text))
-    a.prepare(connection.connection)
+    a.prepare(connection.cursor().connection)
     return a
